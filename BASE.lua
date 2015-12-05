68f374a448a44b20dae0c0d9c8275f99b39c7acb
Bartosz A <arendarskib@gmail.com>
2015-12-05 09:42:18 +0000

    Adding a refreshing mechanism for the actual user scripts to load.

--- BASE.lua
+++ BASE.lua
@@ -1,3 +1,25 @@
+function scandir(directory)
+    local i, t, popen = 0, {}, io.popen
+    for filename in popen('dir "'..directory..'" /b'):lines() do
+    	if string.find(filename, ".lua") ~= nil then
+	        t[i] = filename
+	        i = i + 1
+    	end
+    end
+    return t
+end
+
+local luaFolderDir = debug.GetR1Path() .. "\\..\\scripts\\"
+local allFileNames = scandir(luaFolderDir)
+
+
+for _,fileName in pairs(allFileNames) do 
+	ui.SysMsg("Reloading: " .. fileName);
+	dofile(luaFolderDir .. fileName);
+end
+
+ui.SysMsg("All Lua Files Refreshed")
+
 return { 
     init = function ()
         _G["MyCharacter"] = dofile("..\\core\\MyCharacter.lua")
